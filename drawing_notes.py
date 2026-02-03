e726003ada388917e8aefb8d67bcc911362b7101
atlantisPrototyping <adrian@atlantisprototyping.com>
2026-02-03 11:55:31 +0100

    Update drawing_notes.py

--- drawing_notes.py
+++ drawing_notes.py
@@ -90,7 +90,7 @@ with header_col1:
 
 with header_col2:
     st.markdown('<div class="logo-container">', unsafe_allow_html=True)
-    st.image("logoVerde.png", width=400)
+    st.image("logoVerde.png", width=300)
     st.markdown('</div>', unsafe_allow_html=True)
 
 st.markdown("---")
